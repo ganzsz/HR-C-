84bb1d528581350caf75bada100e7e431c15f2f4
Hans de Rooij <ganzsz555@gmail.com>
2022-10-29 12:35:35 +0200

    Explain floor intersecting algo

--- Eindopdracht/floor.cpp
+++ Eindopdracht/floor.cpp
@@ -5,30 +5,48 @@
 #include "floor.hpp"
 #include <math.h>
 
-/* Create a floor specify the height and the size of a single square */
+/* Create a floor specify the height and the size of a single square. */
 Floor::Floor (float height, float squareSize) : Object(0, height, 0), squareSize(squareSize) {};
+
 /**
+ * We need a point on the plane and a (non normalized) normal to the plane. To achieve this, 
+ * center is defined in such a way that is is both a point on the plane and at the same time
+ * the normal to the plane.
  * 
+ * dot = the dotproduct of the normal and the ray direction. This is used to later find the
+ * point on the ray where the ray intersects. Because we test the ray before the formula we
+ * can know this will happen.
+ * 
+ * w = the diff between the support and the point on the plane. we use it to only get the
+ * component of the ray that we need
+ * 
+ * fac = the amount of ray.distance traveled on the ray where the ray hits the plane
+ * 
+ * u = the ray we get when using fac on the direction
+ * 
+ * P = ray u added to the support (the start of the ray)
+ * 
+ * If P.x < 0 we want to add 1 to the |P.x|/square calculation. This is to 
+ * account for the fact that we want odd squares to be white in the negative x domain.
+ * Otherwise the [0,1] and [-1,0] x values would be the same color on a given y
+ * 
+ * Taken from https://stackoverflow.com/questions/5666222/3d-line-plane-intersection
+ * adapted for our specific usecase
 */
+
 /* Returns true if the given ray hits a white square on the floor*/
 bool Floor::hit(Ray &ray) {
     //the sceen is x,y depth = z
     if(ray.direction.y >=0 ) return false;
     //we don't want to show the floor up to the horizon which is halfway up the screen
-    if(ray.support.x == 0.0 && ray.support.y ==0.0 && ray.support.z == -3.0 && ray.direction.y > -0.1) return false;
+    if(ray.bounces == 0 && ray.direction.y > -0.1) return false;
 
-    // Taken from https://stackoverflow.com/questions/5666222/3d-line-plane-intersection
-    // adapted to use c++
     auto dot = center.dot(ray.direction);
     auto w = ray.support.sub(center);
     auto fac = -center.dot(w)/dot;
     auto u = ray.direction.mul(fac);
     auto P = ray.support.add(u);
-    /**
-     * If P.x < 0 we want to add 1 to the |P.x|/square calculation. This is to 
-     * account for the fact that we want odd squares to be white in the negative x domain.
-     * Otherwise the [0,1] and [-1,0] x values would be the same color on a given y
-    */
+
     auto hit = P.x > 0
         ? (int)floor(abs(P.z)/squareSize) % 2 == (int)floor(abs(P.x)/squareSize) % 2
         : (int)floor(abs(P.z)/squareSize) % 2 == (int)floor((abs(P.x)/squareSize) + 1) % 2;
